7ccf21ecb8493c6d7b5e67331d9683590fdf444d
fg320 <fg320@ic.ac.uk>
2023-12-11 20:53:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,18 @@ setup(
     author='Filippo Gori',
     author_email='f.gori21@imperial.ac.uk',
     description='A data-integrated framework for wake steering control of wind farms.',
-    long_description=open('README.md').read(),
+    long_description='A data-integrated framework for wake steering control of wind farms.',
     long_description_content_type='text/markdown',
     url='https://github.com/fg320/DEASC',
     install_requires=[
-        'GPy>=1.10.0'
+        'numpy>=1.23.5',
+        'GPy>=1.10.0',
+        'floris>=3.4',
+        'TuRBO>=0.0.1'
     ],
+    license='Apache-2.0',
     classifiers=[
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
     ],
 )
